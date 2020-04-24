f7e10e8814b0249798794377f3fa57070cdccd19
TONG <tongza_7470@hotmail.com>
2020-04-24 21:28:58 +0700

    get and read data from array , apply to MapView

--- App.js
+++ App.js
@@ -32,7 +32,7 @@ export default class App extends Component {
 
   render() {
     return (
-          <FlogtingButton/>
+          <MapView/>
     )
   }
 }
--- src/screen/MapView.js
+++ src/screen/MapView.js
@@ -7,12 +7,41 @@ import ActionButton from 'react-native-action-button';
 const height = Dimensions.get('window').height;
 const width = Dimensions.get('window').width;
 
+const markersArray = [
+  {
+    title: 'point A',
+    des:'ร้านขายอาหาร',
+    coordinate: {
+      latitude:37.419499,
+      longitude:-122.080525
+    }
+  },
+  {
+    title: 'point B',
+    des:'ร้านขายรองเท้า',
+    coordinate: {
+      latitude:37.421971,
+      longitude:-122.089740
+    }
+  },
+  {
+    title: 'point C',
+    des:'ร้านขายเสื้อผ้า',
+    coordinate: {
+      latitude:37.426593,
+      longitude:-122.081076
+    }
+  }
+
+]
 export default class Mapiew extends Component {
   constructor(props) {
     super(props);
     this.state = {
       lat:null,
-      lng:null
+      lng:null,
+      coor:null,
+      markers: [],
     };
   }
 
@@ -21,28 +50,71 @@ export default class Mapiew extends Component {
       console.log('position: ', position)
       this.setState({
         lat:position.coords.latitude,
-        lng:position.coords.longitude
+        lng:position.coords.longitude,
+        coor:position.coords
       })
       console.log('coords', position)
       // console.log('longitude', position.coords.longitude)
     }, (err) => {
       console.log('error:', err)
     },
-    { enableHighAccuracy: true, timeout: 100 , maximumAge: 0,})
+    { enableHighAccuracy: true, timeout: 20000 , maximumAge: 2000,})
+  }
+
+  randomColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
+  
+  onMapPress(e) {
+    this.setState({
+      markers: [
+        ...this.state.markers,
+        {
+          coordinate: e.nativeEvent.coordinate,
+          // key: id++,
+        },
+      ],
+    });
   }
 
   render() {
     console.log('-------------------')
     console.log('latitude', this.state.lat)
     console.log('longitude', this.state.lng)
+    console.log('Markers', this.state.markers)
     return (
       <View style={{flex:1, backgroundColor: '#f3f3f3'}}>
-        <MapView ref="map" mapType="terrain" style={styles.map} initialRegion={{latitude:this.state.lat, longitude:this.state.lng,latitudeDelta: 0.0922, longitudeDelta: 0.0421}}>
-          <MapView.Marker coordinate={{latitude: this.state.lat, longitude: this.state.lng}}>
+        <MapView 
+          ref="map" 
+          mapType="terrain" 
+          style={styles.map} 
+          onPress={e => this.onMapPress(e)}
+          initialRegion={{latitude:this.state.lat, 
+                          longitude:this.state.lng,
+                          latitudeDelta: 0.0922, 
+                          longitudeDelta: 0.0421}}
+        >
+        {
+          this.state.markers != null && this.state.markers.map((marker, index) => (
+            <MapView.Marker
+                key = {index}
+                coordinate={marker.coordinate}
+                title={marker.title}
+                description={marker.des}
+            >
+              {console.log('index:', index)}
+              {console.log('test:', marker)}
+              {console.log('coor state:', this.state.coor)}
+            </MapView.Marker>
+          ))
+        }
+          {/* <MapView.Marker coordinate={this.state.coor}>
             <View style={styles.radius}>
               <View style={styles.marker} />
             </View>
-          </MapView.Marker>
+          </MapView.Marker> */}
         </MapView>
       </View>
     );
